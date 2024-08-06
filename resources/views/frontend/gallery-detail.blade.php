d0736dd8f482a48b9dee19d26c3a7ca9a2429af7
M.L. Prajapat <mangilalprajapat3@gmail.com>
2024-08-06 21:35:56 +0530

    :WID Gallery Detail

--- resources/views/frontend/gallery-detail.blade.php
+++ resources/views/frontend/gallery-detail.blade.php
@@ -2,6 +2,25 @@
 @section('meta_title','Our Gallery | '.env('APP_NAME'))
 @section('meta_description','Our Gallery | '.env('APP_NAME'))
 @section('content')
+<section class="gallery">
+    <div class="container">
+        <div class="row">
+            <div class="head">
+                <h2>{{ $gallery->title }}</h2>
+            </div>
+            @php
+                $images = json_decode($gallery->image_paths);
+            @endphp
+            @foreach ($images as $image)
+                <div class="col-lg-3">
+                    <div class="glry_box">
+                        <a class="example-image-link" href="{{ asset($image) }}" data-lightbox="example-set"><img class="example-image" src="{{ asset($image) }}" alt="{{ $gallery->title }}"/></a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</section>
 @endsection
 @section('style')
 <link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/css/lightbox.min.css" rel="stylesheet">
